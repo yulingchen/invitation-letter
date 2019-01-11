0af21c65c30acb5190f00bca1d560abf7c4630e5
yulingchen <yulingchen@vip.qq.com>
2019-01-11 18:52:34 +0800

    update

--- static/js/main.js
+++ static/js/main.js
@@ -1,19 +1,42 @@
 (function () {
   var stage,
     scale,
+    layer_bottom,
+    layer_middle,
+    layer_role,
+    layer_top,
     imageDir = 'static/images/',
     imageManifest = [
-      { src: "canvas-bg.png", id: "canvasBg", position: { x: 0, y: 0 } }, //画布底部背景
+      { src: "bottom/bg.png", id: "bottom_bg", container: "bottom", position: { x: 0, y: 0 } },
+      { src: "middle/floor.png", id: "middle_floor", container: "middle", position: { x: 0, y: 492 } },
+      { src: "middle/welcome.png", id: "middle_welcome", container: "middle", position: { x: 130, y: 152 } },
+      { src: "top/direct.png", id: "top_direct", container: "top", position: { x: -20, y: 95 } },
     ],
-    imageDisplayObjects = {};
+    imageDisplayObjects = {},
+    run_left = document.getElementById('run_left'),
+    run_right = document.getElementById('run_right'),
+    running = false;
 
   //绘制图片
   function drawImage(imgObject) {
     var image = new createjs.Bitmap(imgObject.img);
     image.x = imgObject.pos.x;
     image.y = imgObject.pos.y;
-    image.scale = scale * 0.8
-    stage.addChild(image);
+
+    switch (imgObject.container) {
+      case 'bottom':
+        layer_bottom.addChild(image);
+        break;
+      case 'middle':
+        layer_middle.addChild(image);
+        break;
+      case 'role':
+        layer_role.addChild(image);
+        break;
+      case 'top':
+        layer_top.addChild(image);
+        break;
+    }
     stage.update()
   }
 
@@ -34,7 +57,8 @@
     function _handleFileLoad(event) {
       imageDisplayObjects[event.item.id] = {
         img: event.result,
-        pos: event.item.position
+        pos: event.item.position,
+        container: event.item.container,
       }
     }
 
@@ -42,6 +66,58 @@
     preload.on("complete", _handleComplete);
     preload.on("fileload", _handleFileLoad);
     preload.loadManifest(imageManifest, true);
+  }
+
+  //初始化容器
+  function initContainer() {
+    function _createContainer() {
+      var layer = new createjs.Container()
+      layer.x = 0
+      layer.y = 0
+      stage.addChild(layer);
+      return layer
+    }
+    layer_bottom = _createContainer()
+    layer_middle = _createContainer()
+    layer_role = _createContainer()
+    layer_top = _createContainer()
+  }
+
+  //绑定控制事件
+  function bindControlEvent() {
+    function _run(direct) {
+      (function (running) {
+        setTimeout(function () {
+          console.log('run ' + direct)
+          if (running) {
+            _run(direct)
+          }
+        }, 30)
+      })(running)
+    }
+
+    run_left.addEventListener('touchstart', function (e) {
+      e.preventDefault()
+      running = true
+      _run('left')
+    });
+
+    run_left.addEventListener('touchend', function (e) {
+      e.preventDefault()
+      running = false
+      _run('left')
+    });
+
+    run_right.addEventListener('touchstart', function (e) {
+      e.preventDefault()
+      running = true
+      _run('right')
+    });
+
+    run_right.addEventListener('touchend', function (e) {
+      e.preventDefault()
+      running = false
+    });
   }
 
   //初始化舞台
@@ -53,14 +129,13 @@
     stage = new createjs.Stage("canvas");
     createjs.Touch.enable(stage);
 
-    //加载背景图
-    var preloadBg = new createjs.LoadQueue();
-    preloadBg.addEventListener("fileload", function (event) {
-      scale = h / event.result.height
-      stage.canvas.width = w;
-      stage.canvas.height = h;
-      preloadImages()
-    });
-    preloadBg.loadFile(imageDir + 'canvas-bg.png');
+    stage.canvas.width = 750;
+    stage.canvas.height = 1334;
+    stage.scaleY = (h * 2 / 1334) / 2
+    stage.scaleX = (w * 2 / 750) / 2
+
+    bindControlEvent()
+    initContainer()
+    preloadImages()
   })()
 })()
